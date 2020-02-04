05d837e6b72e69698247923a9265a6f9e46a252b
unknown <bharathreddy885@gmail.com>
2020-02-04 22:14:52 +0530

    added rotuer via npm and added home page and hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 
+
+//Hats page 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+
+
 function App() {
+  //add the swith to render the exact match i.e when we navigate hats page only hats page needs to rendered
   return (
     <div >
-     <HomePage/>
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }

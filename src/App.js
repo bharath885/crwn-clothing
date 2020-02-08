4373a6ebc813f14f7625724e28d9edb15c2ebbb3
unknown <bharathreddy885@gmail.com>
2020-02-08 12:05:54 +0530

    updtin our app with a shop page,collection preivew and collection item component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
-//Hats page 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
 
 
 
@@ -20,7 +16,7 @@ function App() {
     <div >
       <Switch> 
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
